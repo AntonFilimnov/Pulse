e3e3f8f9f73aa2303655c1884261fa0e89efde2f
Anton Filimonov <antonfil108@gmail.com>
2022-12-08 16:20:57 +0400

    tabs added

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  arrows: false,
+                  dots: false
+                }
+            }
+        ]
+    });
+
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    
+            })
+        });
+
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+ });
