c9a8eafa86539926113a5e47baf49e25e424ff48
Anton Filimonov <antonfil108@gmail.com>
2022-12-13 00:59:52 +0400

    added smooth scrool and page up

--- src/js/script.js
+++ src/js/script.js
@@ -113,5 +113,24 @@ $(document).ready(function(){
         return false;
 
     });
+
+    //smoth scroll and pageup
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+
+
+
+    });
+
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
 });    
     
